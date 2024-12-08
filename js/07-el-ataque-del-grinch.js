ee4aa8f151942ee2ec95e69af07587ce4ca1c8f1
Andres Garcia <garcia.andi90@gmail.com>
2024-12-08 01:42:02 -0300

    Adding resolution of 7th

---
+++ js/07-el-ataque-del-grinch.js
@@ -0,0 +1,17 @@
+
+function fixPackages(packages) {
+  const stack = [];
+  const result = packages.split('');
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] === '(') {
+      stack.push(i);
+    } else if (result[i] === ')') {
+      const openIndex = stack.pop();
+      const subPackage = result.slice(openIndex + 1, i).reverse();
+      result.splice(openIndex, i - openIndex + 1, ...subPackage);
+      i = openIndex + subPackage.length - 1;
+    }
+  }
+
+  return result.join('');
+}

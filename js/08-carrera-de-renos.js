6be0438210f13e29833c20bb34d66da7ea6764f9
Andres Garcia <garcia.andi90@gmail.com>
2024-12-11 17:48:38 -0300

    update 8th

--- js/08-carrera-de-renos.js
+++ js/08-carrera-de-renos.js
@@ -1,9 +1,18 @@
 function drawRace(indices, length) {
-  const raceLength = indices.length -1;
+  const createWave = (count) => "~".repeat(count);
   const race = indices.map((indice, index) => {
-    if (indice === 0) { return `${" ".repeat(raceLength-index)}${"~".repeat(length)} /${index + 1}`}
-    if (indice < 0) { const indiciePos= indice * (-1); return `${" ".repeat(raceLength-index)}${"~".repeat(length - indiciePos)}r${"~".repeat(indiciePos-1)} /${index + 1}`}
-    if (indice > 0) { return `${" ".repeat(raceLength-index)}${"~".repeat(indice)}r${"~".repeat((length - indice)-1)} /${index + 1}`}
+    const space = " ".repeat( (indices.length -1 )-index);
+    const position = ` /${index + 1}`;
+    
+    if (indice === 0) return `${space}${"~".repeat(length)}${position}`;
+
+    const absNum = Math.abs(indice);
+    const before = indice < 0 ? length - absNum : indice;
+    const after = indice < 0 ? absNum - 1 : (length - indice)-1
+
+    // if (indice < 0) { const indiciePos= indice * (-1); return `${space}${"~".repeat(length - indiciePos)}r${"~".repeat(indiciePos-1)}${position}`}
+    // if (indice > 0) { return `${space}${"~".repeat(indice)}r${"~".repeat((length - indice)-1)}${position}`}
+    return `${space}${createWave(before)}r${createWave(after)}${position}`
   });
   return race.join("\n");
 }

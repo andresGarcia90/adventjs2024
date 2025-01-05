4c838e1a984fd59ad7881b7871b27ab0757583f9
Andres Garcia <garcia.andi90@gmail.com>
2025-01-05 01:48:42 -0300

    solution 16th

---
+++ js/16-limpiando-nieve.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @returns {string}
+ */
+function removeSnow(s) {
+  let canReduce = true;
+  let newSnow = s;
+  while (canReduce) {
+    canReduce = false;
+    for (let i = 0; i < newSnow.length - 1; i++) {
+      if (newSnow[i] === newSnow[i + 1]) {
+        newSnow = newSnow.slice(0, i) + newSnow.slice(i + 2);
+        canReduce = true;
+        break;
+      }
+    }
+  }
+  return newSnow;
+}
+
+
+console.log(removeSnow('zzz'));

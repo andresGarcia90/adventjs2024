ebb13fdc5243478baf0490dee39e91a57f8b4c41
Andres Garcia <garcia.andi90@gmail.com>
2025-01-04 23:47:26 -0300

    Solution 12

---
+++ js/12-cuanto-cuesta-un-arbol.js
@@ -0,0 +1,33 @@
+/** @param {string} ornaments
+ * @return {number} - The price of the tree
+ */
+function calculatePrice(ornaments) {
+  const items = {
+    '*': 1,
+    'o': 5,
+    '^': 10,
+    '#': 50,
+    '@': 100,
+  };
+
+  let price = 0;
+  for (let i = 0; i < ornaments.length; i++) {
+    const ornament = ornaments[i];
+    if (!items[ornament]) return undefined;
+    let value = items[ornament];
+
+    if (i < ornaments.length - 1) {
+      const nextValueOrnament = items[ornaments[i + 1]];
+      if (!nextValueOrnament) return undefined;
+      value = value < nextValueOrnament ? -value : value;
+    }
+
+    price += value;
+  }
+
+  return price;
+}
+
+const price = calculatePrice('#@Z');
+console.log(price);
+

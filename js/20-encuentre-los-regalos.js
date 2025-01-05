bf8ef027eb598213a43efba1a23211b1f82086f1
Andres Garcia <garcia.andi90@gmail.com>
2025-01-05 02:42:10 -0300

    Solution 20th

---
+++ js/20-encuentre-los-regalos.js
@@ -0,0 +1,37 @@
+/**
+ * @typedef {Record<string, number>} GiftsCount
+ */
+
+/**
+ * @typedef {{ missing: GiftsCount, extra: GiftsCount }} Result
+ */
+
+/**
+ * @param {string[]} received
+ * @param {string[]} expected
+ * @returns {Result}
+ */
+function fixGiftList(received, expected) {
+  const result = {
+    missing: {},
+    extra: {}
+  };
+
+  const items = {};
+  received.forEach(gift => {
+    items[gift] = (items[gift] || 0) + 1;
+  })
+
+  expected.forEach(gift => {
+    items[gift] = (items[gift] || 0) - 1;
+  })
+
+  Object.keys(items).forEach(gift => {
+    if(items[gift] > 0) result.extra[gift] = items[gift];
+    if(items[gift] < 0) result.missing[gift] = Math.abs(items[gift]);
+  });
+
+  return result;
+}
+
+console.log(fixGiftList(['puzzle', 'car', 'doll', 'car'], ['car', 'puzzle', 'doll', 'ball']));

a3e166e3cdd0d566c94d1e317700fbc090cb854c
Andres Garcia <garcia.andi90@gmail.com>
2025-01-08 15:11:39 -0300

    Adding solution of 25th

--- js/25-ejecuta-el-lenguaje.js
+++ js/25-ejecuta-el-lenguaje.js
@@ -3,37 +3,36 @@
  * @returns {number} - The final value after executing the program
  */
 function execute(code) {
-
   let value = 0;
   let i = 0;
   let length = code.length;
 
   while (i < length) {
     const instruction = code[i];
-
     if (instruction === '>') {
       i += 1;
-    } else if (instruction  === '+') {
+    } else if (instruction === '+') {
       value += 1;
       i += 1;
     } else if (instruction === '-') {
       value -= 1;
       i += 1;
-    } else if (instruction === '['){
-      if (value === 0) {
-        while (code[i] !== ']') {
-          i += 1;
-        }
-      } 
+    } else if (instruction === ']') {
+      value = 0;
       i += 1;
-    } else if (instruction === '{') {}
-
-
+    } else if (instruction === '{') {
+      if (value === 0) {
+        const restOfInstruction = code.split('').slice(i);
+        const indexOfBracket = restOfInstruction.findIndex(x => x === '}');
+        i += indexOfBracket + 1;
+      } else {
+        i += 1;
+      }
+    } else if (instruction === '[' || instruction === '}') {
+      i +=1;
+    }
   }
-  
-
   return value;
 }
-  
 
-console.log(execute('+++'));
+console.log(execute('{+}{+}{+}'));
